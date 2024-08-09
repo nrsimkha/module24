8d243d3cde79ca385f6de0e435a976686c061d5f
Nrsimkha <sveta.budanaeva@gmail.com>
2024-08-09 00:38:44 -0300

    Forth commit: two additional functions

--- 24.6.6.go
+++ 24.6.6.go
@@ -6,10 +6,21 @@ func plus(a int, b int) int {
 
 	return a + b
 }
-
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+func plusPlusPlus(a, b, c, d int) int {
+	return a + b + c + d
+}
 func main() {
 
 	res := plus(1, 2)
 	fmt.Println("1+2 =", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+
+	res = plusPlusPlus(1, 2, 3, 4)
+	fmt.Println("1+2+3+4 =", res)
 
 }
